1e6bf7e6b66002f242ba8dfe281b95fa9ab4f174
sayazian <sahar.ayazian@gmail.com>
2024-12-15 10:15:41 -0800

    included feedbacks from Assignment 2

--- src/com/coderscampus/assignment/UserLoginApplication.java
+++ src/com/coderscampus/assignment/UserLoginApplication.java
@@ -13,13 +13,14 @@ public class UserLoginApplication {
     }
 
     private static void promptTheUser(UserService userService) {
+        Scanner input = new Scanner(System.in);
         String[] userInput;
         int numberOfPrompts = 0;
         int validUserIndex = -1;
 
         while ((validUserIndex < 0) && (numberOfPrompts < 5)) {
             numberOfPrompts++;
-            userInput = getUsernamePassword();
+            userInput = getUsernamePassword(input);
             validUserIndex = userService.isUserValid(userInput);
             if ((validUserIndex < 0) && (numberOfPrompts < 4)) {
                 System.out.println("Invalid username/password.");
@@ -30,11 +31,11 @@ public class UserLoginApplication {
         } else {
             System.out.println("Too many failed login attempts, you are now locked out.");
         }
+        input.close();
     }
 
-    private static String[] getUsernamePassword() {
+    private static String[] getUsernamePassword(Scanner input) {
         String[] userInput = new String[2];
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter your email:");
         userInput[0] = input.nextLine();
         System.out.println("Enter your password:");
