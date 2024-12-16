70e4c10065d77c3342a48ed66ac1d85e506ee324
sayazian <sahar.ayazian@gmail.com>
2024-12-16 09:19:17 -0800

    included feedbacks from Kevin. Also added .idea and .iml files.

--- src/com/coderscampus/assignment/UserLoginApplication.java
+++ src/com/coderscampus/assignment/UserLoginApplication.java
@@ -1,7 +1,6 @@
 package com.coderscampus.assignment;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class UserLoginApplication {
 
@@ -9,38 +8,7 @@ public class UserLoginApplication {
         String fileName = "./data.txt";
         UserService userService = new UserService();
         userService.loadUsersInformation(fileName);
-        promptTheUser(userService);
-    }
-
-    private static void promptTheUser(UserService userService) {
-        Scanner input = new Scanner(System.in);
-        String[] userInput;
-        int numberOfPrompts = 0;
-        int validUserIndex = -1;
-
-        while ((validUserIndex < 0) && (numberOfPrompts < 5)) {
-            numberOfPrompts++;
-            userInput = getUsernamePassword(input);
-            validUserIndex = userService.isUserValid(userInput);
-            if ((validUserIndex < 0) && (numberOfPrompts < 4)) {
-                System.out.println("Invalid username/password.");
-            }
-        }
-        if (validUserIndex > 0) {
-            System.out.println("Welcome " + userService.getUserName(validUserIndex) + "!");
-        } else {
-            System.out.println("Too many failed login attempts, you are now locked out.");
-        }
-        input.close();
-    }
-
-    private static String[] getUsernamePassword(Scanner input) {
-        String[] userInput = new String[2];
-        System.out.println("Enter your email:");
-        userInput[0] = input.nextLine();
-        System.out.println("Enter your password:");
-        userInput[1] = input.nextLine();
-        return userInput;
+        userService.promptTheUser(userService);
     }
 
 }
--- src/com/coderscampus/assignment/UserService.java
+++ src/com/coderscampus/assignment/UserService.java
@@ -3,14 +3,11 @@ package com.coderscampus.assignment;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class UserService {
 
     final User[] users = new User[10];
-
-    public String getUserName(int index) {
-        return users[index].getName();
-    }
 
     public User createUser(String[] stringInput) {
         User user = new User();
@@ -37,15 +34,46 @@ public class UserService {
         return input.split(",");
     }
 
-    public int isUserValid(String[] userInput) {
+    public User isUserValid(String[] userInput) {
 
-        for (int i = 0; i < users.length; i++) {
-            if (!(users[i] == null)) {
-                if ((userInput[0].equals(users[i].getUsername())) && (userInput[1].equals(users[i].getPassword())))
-                    return i;
+        for (User user : users) {
+            if (!(user == null)) {
+                if ((userInput[0].equalsIgnoreCase(user.getUsername())) && (userInput[1].equals(user.getPassword())))
+                    return user;
             }
         }
-        return -1;
+        return null;
+    }
+
+    public void promptTheUser(UserService userService) {
+        Scanner input = new Scanner(System.in);
+        String[] userInput;
+        int numberOfPrompts = 0;
+        User validUser = null;
+
+        while ((validUser == null) && (numberOfPrompts < 5)) {
+            numberOfPrompts++;
+            userInput = getUsernamePassword(input);
+            validUser = userService.isUserValid(userInput);
+            if ((validUser == null) && (numberOfPrompts < 4)) {
+                System.out.println("Invalid username/password.");
+            }
+        }
+        if (validUser != null) {
+            System.out.println("Welcome " + validUser.getName() + "!");
+        } else {
+            System.out.println("Too many failed login attempts, you are now locked out.");
+        }
+        input.close();
+    }
+
+    private static String[] getUsernamePassword(Scanner input) {
+        String[] userInput = new String[2];
+        System.out.println("Enter your email:");
+        userInput[0] = input.nextLine();
+        System.out.println("Enter your password:");
+        userInput[1] = input.nextLine();
+        return userInput;
     }
 
 }
